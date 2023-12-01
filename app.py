a860a88062c9b9c5917b699083129b8d0a761665
Lupe <d.lupescu@hotmail.fi>
2023-12-01 13:50:46 +0200

    Lisätty kuvia

--- app.py
+++ app.py
@@ -9,11 +9,17 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Tietokantayhteys
-config.conn = mysql.connector.connect(
-         host=os.environ.get('HOST'),
-         port= 3306,
-         database=os.environ.get('DB_NAME'),
-         user=os.environ.get('DB_USER'),
-         password=os.environ.get('DB_PASS'),
-         autocommit=True
-         )
+
+try:
+    config.conn = mysql.connector.connect(
+        host=os.environ.get('HOST'),
+        port=3306,
+        database=os.environ.get('DB_NAME'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASS'),
+        autocommit=True
+    )
+
+except Exception as e:
+   print(e)
+   print(type(e))
