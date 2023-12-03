ddecb580b19f1cdb5c4ae2f7c47776a45c45c05d
janneesa <janne.savinainen@hotmail.com>
2023-12-03 15:09:09 +0200

    toimii

--- static/app.py
+++ static/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Tietokantayhteys
 
 try:
-    config.conn = mysql.connector.connect(
+    conn = mysql.connector.connect(
         host='mysql.metropolia.fi',
         port=3306,
         database='danielup',
@@ -24,15 +24,15 @@ except Exception as e:
    print(e)
    print(type(e))
 
-def hae_kaikki_kohteet(pelaaja):
+def hae_kaikki_kohteet():
 
     sql = f'''SELECT airport.id, airport.fantasia_nimi, airport.latitude_deg, airport.longitude_deg 
-              FROM airport WHERE airport.id != {pelaaja.sijainti}'''
-    kursori = yhteys.cursor(dictionary=True)
+              FROM airport'''
+    kursori = conn.cursor(dictionary=True)
     kursori.execute(sql)
     lista = kursori.fetchall()
     #for nimi in lista:
         #print(nimi)
     return lista
 
-hae_kaikki_kohteet()
+print(hae_kaikki_kohteet())
