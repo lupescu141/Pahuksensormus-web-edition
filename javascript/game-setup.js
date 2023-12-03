6a2d579315b54a93a941a60b468b3f3dff051801
janneesa <janne.savinainen@hotmail.com>
2023-12-03 15:44:12 +0200

    java

---
+++ javascript/game-setup.js
@@ -0,0 +1,6 @@
+// Start new game new player name
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  const pelaaja_nimi = document.querySelector('#player-input').value;
+  document.querySelector('#player-modal').classList.add('hide');
+});
--- static/app.py
+++ static/app.py
@@ -24,6 +24,36 @@ except Exception as e:
    print(e)
    print(type(e))
 
+
+class Pelaaja:
+    def __init__(self, peli_id, pelaaja_nimi, pelaaja_sijainti, menneet_paivat, pelaaja_hp, pelaaja_maxhp,
+                 pelaaja_suojaus, pelaaja_isku, pelaaja_taitopiste, pelaaja_max_taitopiste,
+                 onko_sormus, sormus_sijainti):
+        self.id = peli_id
+        self.nimi = pelaaja_nimi
+        self.sijainti = pelaaja_sijainti
+        self.menneet_paivat = menneet_paivat
+        self.hp = pelaaja_hp
+        self.maxhp = pelaaja_maxhp
+        self.suojaus = pelaaja_suojaus
+        self.isku = pelaaja_isku
+        self.taitopiste = pelaaja_taitopiste
+        self.max_taitopiste = pelaaja_max_taitopiste
+        self.onko_sormus = onko_sormus
+        self.sormus_sijainti = sormus_sijainti
+
+class Vihollinen:
+    def __init__(self, vihollinen_id, vihollinen_nimi, vihollinen_hp, vihollinen_maxhp, vihollinen_suojaus, vihollinen_isku):
+        self.id = vihollinen_id
+        self.nimi = vihollinen_nimi
+        self.hp = vihollinen_hp
+        self.maxhp = vihollinen_maxhp
+        self.suojaus = vihollinen_suojaus
+        self.isku = vihollinen_isku
+
+
+
+
 def hae_kaikki_kohteet():
 
     sql = f'''SELECT airport.id, airport.fantasia_nimi, airport.latitude_deg, airport.longitude_deg 
