e053db98a1028cadd0a16e64dea0024547d58a95
janneesa <janne.savinainen@hotmail.com>
2023-12-03 17:04:18 +0200

    ..

--- javascript/game-setup.js
+++
@@ -1,6 +0,0 @@
-// Start new game new player name
-document.querySelector('#player-form').addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  const pelaaja_nimi = document.querySelector('#player-input').value;
-  document.querySelector('#player-modal').style.display='none';
-});
