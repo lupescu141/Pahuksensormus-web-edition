85a33595f3b977114a3f574d15f58c83f3136c4d
janneesa <janne.savinainen@hotmail.com>
2023-12-03 15:03:09 +0200

    def ei toimi

--- static/app.py
+++ static/app.py
@@ -23,3 +23,16 @@ try:
 except Exception as e:
    print(e)
    print(type(e))
+
+def hae_kaikki_kohteet(pelaaja):
+
+    sql = f'''SELECT airport.id, airport.fantasia_nimi, airport.latitude_deg, airport.longitude_deg 
+              FROM airport WHERE airport.id != {pelaaja.sijainti}'''
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    lista = kursori.fetchall()
+    #for nimi in lista:
+        #print(nimi)
+    return lista
+
+hae_kaikki_kohteet()
