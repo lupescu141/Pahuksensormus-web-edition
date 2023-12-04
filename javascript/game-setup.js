54161b7dbacaa5c28f546ad79356dd9d4fe469dd
janneesa <janne.savinainen@hotmail.com>
2023-12-04 15:38:00 +0200

    päävalikon tekoa

--- javascript/game-setup.js
+++ javascript/game-setup.js
@@ -5,14 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+// Hakee nimet tietokannasta
+async function hae_nimet() {
+  // Hakee Flask tietokannasta nimet
+  const vastaus = await fetch('http://localhost:5000/hae_pelaaja_nimet');
+  return await vastaus.json();
+}
+
 // Tämä hoitaa uuden pelin aloittamisen
-function avaa_uusipeli_valikko() {
+async function avaa_uusipeli_valikko() {
   // Piilota paavalikko
   const paavalikko = document.querySelector('.valikko');
   paavalikko.style.display = 'none';
 
   const uusi_peli_valikko = document.querySelector('.uusi-peli-valikko');
   uusi_peli_valikko.style.display = 'flex';
+
+  const varatut_nimet = await hae_nimet();
+  const varatut_nimet_lista = [];
+  for (let varattu_nimi of varatut_nimet) {
+    let varattu_nimi_caps = varattu_nimi.pelaaja_nimi.toUpperCase();
+    varatut_nimet_lista.push(varattu_nimi_caps);
+  }
+  console.log(varatut_nimet_lista);
 
   // Etsi lomake ja lisää sille submit-kuuntelija
   const uusi_peli_form = document.getElementById('uusi-peli-form');
@@ -22,15 +37,23 @@ function avaa_uusipeli_valikko() {
     // Tässä vaiheessa voit käsitellä pelaajan nimen ja aloittaa pelin
     const pelaaja_nimi_elementti = document.getElementById('pelaajan-nimi');
     const pelaaja_nimi = pelaaja_nimi_elementti.value;
-    console.log('Aloita peli pelaajalla:', pelaaja_nimi);
 
-    // Tyhjennä pelaajan nimi
-    pelaaja_nimi_elementti.value = '';
+    if (varatut_nimet_lista.includes(pelaaja_nimi.toUpperCase())) {
+      console.log('nimi varattu');
+      // Tyhjennä pelaajan nimi
+      pelaaja_nimi_elementti.value = '';
+      pelaaja_nimi_elementti.placeholder = `${pelaaja_nimi} on varattu`;
+    } else {
+      console.log('Aloita peli pelaajalla:', pelaaja_nimi);
 
-    uusi_peli_valikko.style.display = 'none';
+      // Tyhjennä pelaajan nimi
+      pelaaja_nimi_elementti.value = '';
 
-    const vasen_puoli = document.querySelector('.vasen-puoli');
-    vasen_puoli.style.display = 'flex';
+      uusi_peli_valikko.style.display = 'none';
+
+      const vasen_puoli = document.querySelector('.vasen-puoli');
+      vasen_puoli.style.display = 'flex';
+    }
   });
 }
 
@@ -45,9 +68,7 @@ async function avaa_lataapeli_valikko() {
   lataapeli_valikko.style.display = 'flex';
 
   // Hakee Flask tietokannasta tallennukset
-  const vastaus = await fetch('http://localhost:5000/hae_pelaaja_nimet');
-  const data = await vastaus.json();
-  console.log(data);
+  data = await hae_nimet();
 
   // Esitetään tallenukset nappeina
   for (const pelaaja of data) {
