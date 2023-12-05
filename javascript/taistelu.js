5dcfbf761be0bae88b46a4b558a7ec66b8866016
janneesa <janne.savinainen@hotmail.com>
2023-12-05 20:14:39 +0200

    quickfix

--- javascript/taistelu.js
+++ javascript/taistelu.js
@@ -1,9 +1,41 @@
+// Aloitetaan taistelu. pelaaja-status siirretään alaspäin jotta arvot pysyvät samoina
 function taistelu_testi() {
   document.querySelector('.vasen-puoli').style.display = 'none';
   document.querySelector('.oikea-puoli').style.display = 'none';
   document.querySelector('.vasen-puoli-taistelu').style.display = 'block';
   document.querySelector('.oikea-puoli-taistelu').style.display = 'block';
 
+  const pelaaja_status = document.querySelector('.pelaaja-status');
+  const taistelu_rivi = document.querySelector('.taistelu-rivi');
+
+  // Piilota napit
+  const napit = pelaaja_status.querySelectorAll('.nappi');
+  napit.forEach(nappi => {
+    nappi.style.display = 'none';
+  });
+
+  // Lisää pelaaja_status taistelu_rivi:n ensimmäiseksi lapseksi
+  taistelu_rivi.insertBefore(pelaaja_status, taistelu_rivi.firstChild);
+}
+
+// Kun taistelu loppuu siirretään pelaajan status takaisin
+function lopeta_taistelu() {
+  document.querySelector('.vasen-puoli-taistelu').style.display = 'none';
+  document.querySelector('.oikea-puoli-taistelu').style.display = 'none';
+  document.querySelector('.vasen-puoli').style.display = 'flex';
+  document.querySelector('.oikea-puoli').style.display = 'flex';
+
+  const pelaaja_status = document.querySelector('.pelaaja-status');
+  const pelaaja_info = document.querySelector('.pelaaja-info');
+
+  // Piilota napit
+  const napit = pelaaja_status.querySelectorAll('.nappi');
+  napit.forEach(nappi => {
+    nappi.style.display = 'block';
+  });
+
+  // Lisää pelaaja_status taistelu_rivi:n ensimmäiseksi lapseksi
+  pelaaja_info.appendChild(pelaaja_status);
 }
 
 function naytaValikko(valikko) {
