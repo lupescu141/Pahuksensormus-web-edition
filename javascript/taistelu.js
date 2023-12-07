1561a41073d8d64a1ea63a7562c31707d3db431e
Lupe <d.lupescu@hotmail.fi>
2023-12-08 00:33:38 +0200

    css muokattu, pelaajan kuvat parannettu

--- javascript/taistelu.js
+++ javascript/taistelu.js
@@ -6,6 +6,8 @@ let random_vihollinen
 //hakee elementit
 const vihollinen_nimi = document.getElementById('vihollinen_nimi');
 const vihollinen_hp =document.getElementById('vihollinen-hp');
+const taisteluloki = document.getElementById("taistelu-loki");
+const pelaaja_hp = document.getElementById("pelaaja-hp")
 
 // Haetaan random vihollinen
 async function hae_random_vihollinen_tietokannasta() {
@@ -14,15 +16,14 @@ async function hae_random_vihollinen_tietokannasta() {
   random_vihollinen = await random_vihollinen.json();
 
   document.getElementById('vihollinen_nimi').textContent = random_vihollinen.vihollinen_nimi;
-  document.getElementById('vihollinen-hp').textContent = random_vihollinen.vihollinen_hp;
+  vihollinen_hp.textContent = random_vihollinen.vihollinen_hp;
+  document.querySelector(".vihollinen-kuva").style.backgroundImage = `url("../static/images/viholliset/${random_vihollinen.vihollinen_nimi.toLocaleLowerCase()}.png")`;
 
   return random_vihollinen;
 }
 
 
 const taistelu = async () => {
-  //hakee taistelulokin tekstialueen
-  const taisteluloki = document.getElementById("taistelu-loki");
 
   //hakee random vihollisen
   let vihollinen = await hae_random_vihollinen_tietokannasta();
@@ -55,22 +56,40 @@ const taistelu = async () => {
 
                 taisteluloki.value += `\nTeit viholliseen ${isku} vahinkoa!`
                 taisteluloki.scrollTop = taisteluloki.scrollHeight;
-                vihollinen_hp.innerHTML = vihollinen.vihollinen_hp;
+                vihollinen_hp.textContent = vihollinen.vihollinen_hp;
             }
 
             else {
                 taisteluloki.value += `\n${vihollinen.vihollinen_nimi} väisti iskusi!`
                 taisteluloki.scrollTop = taisteluloki.scrollHeight;
             }
+
+            vihollisen_vuoro(vihollinen)
+
         }
     })
 
 }
 
-const vihollisen_vuoro = () => {
+const vihollisen_vuoro = (vihollinen) => {
 
     if (vihollinen.vihollinen_hp > 0){
 
+        const isku_osuma = (Math.floor(Math.random() * 21) + 1) + 2;
+
+        if (isku_osuma >= pelaaja_olio.pelaaja_suojaus) {
+                const isku = (Math.floor(Math.random() * vihollinen.vihollinen_isku) + 1) + 2;
+                pelaaja_olio.pelaaja_hp -= isku
+
+                taisteluloki.value += `\n${vihollinen.vihollinen_nimi} teki sinuun ${isku} vahinkoa!`
+                taisteluloki.scrollTop = taisteluloki.scrollHeight;
+                pelaaja_hp.innerHTML = pelaaja_olio.pelaaja_hp;
+            }
+
+            else {
+                taisteluloki.value += `\nVäistit vihollisen iskun!`
+                taisteluloki.scrollTop = taisteluloki.scrollHeight;
+            }
     }
 }
 
