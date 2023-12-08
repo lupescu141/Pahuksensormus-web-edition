506b6cc855a71b726e7b9ac37a19d3386a15a6f0
gitober <birgitta.oberg@metropolia.fi>
2023-12-08 15:07:40 +0200

    korjattu tekstejä

--- javascript/mekaniikat.js
+++ javascript/mekaniikat.js
@@ -28,16 +28,17 @@ async function hae_inventaario() {
 }
 
 
-async function hae_random_bossi() {
 // Hakee Flask tietokannasta bossin
+async function hae_random_bossi() {
   const response = await fetch(`http://localhost:5000/hae_random_bossi`);
   const vastaus = await response.json()
   console.log(vastaus)
   return vastaus
 }
 
-async function hae_esine() {
+
 // Hakee Flask tietokannasta esineen
+async function hae_esine() {
   const response = await fetch(`http://localhost:5000/hae_esine`);
   const vastaus = await response.json()
   console.log(vastaus)
