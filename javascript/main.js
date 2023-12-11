b73f52338ef848a6875f2f81fd77b80eb48441e1
janneesa <janne.savinainen@hotmail.com>
2023-12-11 22:19:16 +0200

    elementtien haku mainiin

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,25 @@
+// Luodaan pelaajalle muuttuja
+let pelaaja_olio;
+
+let pelaaja_inventaario;
+
+let pelaaja_taidot;
+
+// Päävalikon elementtejä
+const paavalikko = document.querySelector('.valikko');
+const vasen_puoli = document.querySelector('.vasen-puoli');
+const oikea_puoli = document.querySelector('.oikea-puoli');
+const lataapeli_valikko = document.querySelector('.lataapeli-valikko');
+const uusipeli_valikko = document.querySelector('.uusi-peli-valikko');
+const ennatukset = document.querySelector('.ennatykset')
+const taulukko = document.querySelector('.top-lista');
+// Loki elementit
 const textarea = document.getElementById('loki');
+const taisteluloki = document.getElementById('taistelu-loki');
+// Kartta elementit
 const kartta = document.querySelector('.kartta');
 const kartta_nappi = kartta.querySelectorAll('.kartta-nappi-kuva');
-//hakee taisteluvalikon napit
+// Taistelu elementit
 const hyokkaa = document.getElementById('hyokkaa');
 const taidot = document.getElementById('taidot');
 const esineet = document.getElementById('esineet');
@@ -15,10 +33,30 @@ const esine2 = document.getElementById('esine2');
 const esine3 = document.getElementById('esine3');
 const vihollinen_nimi = document.getElementById('vihollinen_nimi');
 const vihollinen_hp = document.getElementById('vihollinen-hp');
-const taisteluloki = document.getElementById('taistelu-loki');
 const pelaaja_hp = document.getElementById('pelaaja-hp');
 const pelaaja_status = document.querySelector('.pelaaja-status');
 const taistelu_rivi = document.querySelector('.taistelu-rivi');
 const pelaaja_info = document.querySelector('.pelaaja-info');
 
+// Hoitaa alkuvalikon esittelyn sivun latautuessa
+document.addEventListener('DOMContentLoaded', async function() {
+  // Etsi vasemman puolen elementti ja aseta sille display: none;
+  document.querySelector('.vasen-puoli').style.display = 'none';
+  document.querySelector('.kartta').style.display = 'none';
+  document.getElementById('loki').value = '';
 
+  const response = await fetch(`http://localhost:5000/hae_ennatukset`);
+  const vastaus = await response.json();
+  console.log(vastaus);
+
+  for (const pisteet of vastaus) {
+    const rivi = taulukko.insertRow();
+
+    const solu1 = rivi.insertCell(0);
+    solu1.textContent = pisteet.pelaaja_nimi;
+
+    const solu2 = rivi.insertCell(1);
+    solu2.textContent = pisteet.menneet_paivat;
+  }
+
+});
